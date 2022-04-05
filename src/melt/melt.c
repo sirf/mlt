50d297b9d1ee4f3c32fb3845e0fb3c9f0b55e780
Johan Westerlund <johan.westerlund@codemill.se>
2022-04-05 13:33:29 +0200

    write status to named pipe

--- src/melt/melt.c
+++ src/melt/melt.c
@@ -28,6 +28,9 @@
 #include <limits.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include <framework/mlt.h>
 
@@ -555,6 +558,11 @@ static JitControl *read_control() {
 static void write_status(JitStatus *const jit_status) {
     static char *buf = NULL;
     static int buf_len = 0;
+	static int fd = -1;
+
+	if (fd < 0) {
+		fd = open("/tmp/jit-status", O_WRONLY);
+	}
 
     int len = jit_status__get_packed_size(jit_status) + 4;
 	if (len < 5) {
@@ -574,7 +582,7 @@ static void write_status(JitStatus *const jit_status) {
     jit_status__pack(jit_status, b + 4);
     *((int*) b) = len - 4;
     while (len) {
-        const int w = write(STDERR_FILENO, b, len);
+        const int w = write(fd, b, len);
         if (w < 1) {
             exit(2);
         }
